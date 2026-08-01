80a44110ec8df61e2735d3fd85663c9e74b63b14
VingerChan <2741232639@qq.com>
2026-08-01 22:29:08 +0800

    优化推荐功能

--- VinShop/utils/recommend.py
+++ VinShop/utils/recommend.py
@@ -1,5 +1,6 @@
 from VinShop.settings import POPULAR_LIMIT
 from apps.goods.models import SKU
+import random
 def get_popular_skus():
-    skus = SKU.objects.filter(is_launched=True).order_by('-sales','-comments')[:POPULAR_LIMIT]
-    return skus
+    pool = list(SKU.objects.filter(is_launched=True).order_by('-sales','-comments')[:POPULAR_LIMIT])
+    return random.sample(pool,min(POPULAR_LIMIT,len(pool)))
