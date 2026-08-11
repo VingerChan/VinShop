deb999cbb5dd8913dd1bde9c5e7235a3e21a1ed5
VingerChan <2741232639@qq.com>
2026-08-11 18:11:07 +0800

    订单模块-修复OrderInfo模型

--- VinShop/apps/orders/models.py
+++ VinShop/apps/orders/models.py
@@ -4,14 +4,15 @@ from apps.users.models import User,Address
 from apps.goods.models import SKU
 class OrderInfo(BaseModel):
     # ENUM Dict是给程序员看的，CHOICES只认元组
+    # CHOICES 给Django校验/admin 用： 必须(存库值，中文显示名)
     # if order.status == OrderInfo.ORDER_STATUS_ENUM["UNPAID"]:
     PAY_METHODS_ENUM = {
         'CASH': 1,
         'ALIPAY': 2
     }
     PAY_METHODS_CHOICES = (
-        ('CASH', 1),
-        ('ALIPAY', 2),
+        ('1','货到付款'),
+        ('2','支付宝'),
     )
     STATUS_ENUM = {
         'UNPAID': 1,  # 待支付
@@ -22,12 +23,12 @@ class OrderInfo(BaseModel):
         'CANCELED': 6  # 已取消
     }
     STATUS_CHOICES = (
-        ('待支付', 1),
-        ('待发货', 2),
-        ('待收货', 3),
-        ('待评价', 4),
-        ('已完成', 5),
-        ('已取消', 6),
+        (1,'待支付'),
+        (2,'待发货'),
+        (3,'待收货'),
+        (4,'待评价'),
+        (5,'已完成'),
+        (6,'已取消'),
     )
     order_id = models.CharField(max_length=64,primary_key=True,verbose_name='订单号')
     user = models.ForeignKey(User,on_delete=models.PROTECT,related_name='orders',verbose_name='用户')
@@ -43,8 +44,8 @@ class OrderInfo(BaseModel):
     """
     total_amount = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='商品总价')
     freight = models.DecimalField(max_digits=10,decimal_places=2,default=10,verbose_name='运费')
-    pay_method = models.SmallIntegerField(default=1,choices=PAY_METHODS_CHOICES,verbose_name='支付方式')
-    status = models.SmallIntegerField(default=1,choices=STATUS_CHOICES,verbose_name='订单状态')
+    pay_method = models.SmallIntegerField(default=PAY_METHODS_ENUM['CASH'],choices=PAY_METHODS_CHOICES,verbose_name='支付方式')
+    status = models.SmallIntegerField(default=STATUS_ENUM['UNPAID'],choices=STATUS_CHOICES,verbose_name='订单状态')
     class Meta:
         db_table = 'tb_order_info'
         verbose_name = '订单信息'
