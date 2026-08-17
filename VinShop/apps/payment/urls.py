db4b2ad3a1e8cb069853f70814571a7a0c80e8b1
VingerChan <2741232639@qq.com>
2026-08-17 14:17:29 +0800

    支付模块-支付宝主动查询订单状态(支付成功修改订单status)

--- VinShop/apps/payment/urls.py
+++ VinShop/apps/payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from apps.payment.views import AlipayURLView
+from apps.payment.views import AlipayURLView,AlipayQueryView
 urlpatterns = [
     path('payment/alipay/',AlipayURLView.as_view()),
+    path('payment/alipay/status/',AlipayQueryView.as_view()),
 ]
--- VinShop/apps/payment/views.py
+++ VinShop/apps/payment/views.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from alipay import AliPayException, AliPayValidationError
+from django.db import transaction
 from django.shortcuts import render
 from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
@@ -6,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from utils.alipay import get_alipay
 from apps.orders.models import OrderInfo
+from apps.payment.models import Payment
 
 class AlipayURLView(APIView):
     permission_classes = [IsAuthenticated]
@@ -34,3 +38,43 @@ class AlipayURLView(APIView):
         )
         alipay_url = settings.ALIPAY_URL + '?' + order_string
         return Response({'alipay_url':alipay_url})
+
+# 服务端向支付宝 查询交易状态
+class AlipayQueryView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        order_id = request.query_params.get('order_id','')
+        user = request.user
+        # 校验订单号是否存在 是否属于当前用户
+        try:
+            order = OrderInfo.objects.get(user=user,order_id=order_id)
+        except OrderInfo.DoesNotExist:
+            return Response({'message':'订单不存在'},status=status.HTTP_400_BAD_REQUEST)
+        alipay = get_alipay()
+        try:
+            result = alipay.server_api(
+                "alipay.trade.query",
+                biz_content={
+                    'out_trade_no': order.order_id,
+                }
+            )
+        except (AliPayException,AliPayValidationError):    # 交易不存在/验签失败
+            return Response({'paid':False,'trade_status':None})
+        trade_status = result.get('trade_status')
+        # 交易状态
+        if trade_status in ('TRADE_SUCCESS','TRADE_FINISHED'):
+            with transaction.atomic():
+                order = OrderInfo.objects.select_for_update().get(order_id=order_id,user=user)
+                if order.status == OrderInfo.STATUS_ENUM['UNPAID']:
+                    Payment.objects.update_or_create(
+                        order=order,    # 查询条件
+                        defaults = {
+                            'amount' : Decimal(result.get('total_amount')),
+                            'trade_no' : result.get('trade_no'),
+                        }
+                    )
+                    order.status = OrderInfo.STATUS_ENUM['UNSEND']
+                    order.save(update_fields=['status'])
+            return Response({'paid':True,'trade_no':result.get('trade_no')})
+        # 如果交易失败
+        return Response({'paid':False,'trade_status':trade_status})
